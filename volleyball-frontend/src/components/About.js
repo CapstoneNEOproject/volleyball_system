a893bae4249ac9ce493deb9832de5f7d834a3874
Jacob Dunay <JacobJDunay@gmail.com>
2024-11-10 19:44:24 -0500

    New Pages & Page Animation

---
+++ volleyball-frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PageStyles.css';
+
+const About = () => {
+    return (
+        <div className="page-container">
+            <h1>About Us</h1>
+            <p>The Northeast Ohio Recreational Volleyball Group is a community-driven organization focused on bringing volleyball enthusiasts together for friendly games and competitions. Join us to improve your skills, meet new people, and enjoy the game of volleyball!</p>
+        </div>
+    );
+};
+
+export default About;
