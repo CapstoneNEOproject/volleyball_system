5b83b3dda37f8b6190e978a8342563dc12023d63
Daniel Koirala <51556023+DanielKoirala@users.noreply.github.com>
2024-11-14 02:22:08 -0500

    fixed import

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from .views import register_user
 from rest_framework.routers import DefaultRouter
 from .views import GameViewSet, TeamViewSet
 
@@ -8,5 +9,6 @@ router.register(r'games', GameViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('games/upcoming/', GameViewSet.as_view({'get': 'upcoming_games'}), name='upcoming-games')
+    path('games/upcoming/', GameViewSet.as_view({'get': 'upcoming_games'}), name='upcoming-games'),
+    path('api/register/', register_user, name='register_user'),
 ]
