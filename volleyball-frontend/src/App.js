5a2644a9623ad588890d869afd25dea51613139c
Abhishek Pandit <apandit3@kent.edu>
2024-11-14 11:10:56 -0500

    header margin and padding changes

--- volleyball-frontend/src/App.js
+++ volleyball-frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header/>
         <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
