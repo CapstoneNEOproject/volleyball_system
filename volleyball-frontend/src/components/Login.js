109cdba12be0246dd430e36187bd3525065b2ed7
Abhishek Pandit <apandit3@kent.edu>
2024-11-15 12:31:05 -0500

    changes to login page

--- volleyball-frontend/src/components/Login.js
+++ volleyball-frontend/src/components/Login.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import "../PageStyles.css";
+import Signup from "./Signup.js";
+
+import { Link, useLocation } from "react-router-dom";
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const location = useLocation();
 
   const handleLogin = (e) => {
     e.preventDefault();
     alert("Login form submitted!");
+    console.log(email, password);
     // Add authentication logic here
+    
+    // clearing up the form
+    setEmail("");
+    setPassword("");
+
   };
 
   return (
@@ -29,6 +40,11 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+        {/* <a href="Signup"><i><b>Create new account?</b></i></a> */}
+        <Link to="/signup" className="new-account">
+          <i><b>Create new account?</b></i>
+        </Link>
+        
         <button type="submit">Login</button>
       </form>
     </div>
