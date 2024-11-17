15424fc9c43c4059d02608b4863623483d9916db
Abhishek Pandit <apandit3@kent.edu>
2024-11-17 05:35:23 -0500

    redesigned the contact page:

--- volleyball-frontend/src/components/Contact.js
+++ volleyball-frontend/src/components/Contact.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../PageStyles.css';
+import CallImage from "../assets/call.png";
+import MailImage from "../assets/mail.png";
 
 const Contact = () => {
+
     return (
-        <div className="page-container">
+        <div >
             <h1>Contact Us</h1>
-            <p>Email: contact@norvolleyball.org</p>
-            <p>Phone: (555) 123-4567</p>
-            <p>We would love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out.</p>
+            <p className='feedback'>We would love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out.</p>
+            <div className="contacts">
+                <div className="contact1">
+                    <h2>Brian Valore</h2>
+                    <div className="contact-position">
+                        <img src={MailImage} alt="call-image" className='mail-image' />
+                        <h3><a href="mailto:bvman15@gmail.com" target='_blank' rel="noreferrer" >bvman15@gmail.com</a></h3>
+                    </div>
+                    <div className="contact-position">
+                        <img src={CallImage} alt="call-image" className='call-image' />
+                        <h3>(216) 401-6362</h3>
+                    </div>
+                </div>
+                <div className="contact2">
+                    <h2>Steve Colegrove</h2>
+                    <div className="contact-position">
+                        <img src={MailImage} alt="call-image" className='mail-image' />
+                        <h3><a href="mailto:steve.neoathletics@gmail.com" target='_blank' rel="noreferrer" >steve.neoathletics@gmail.com</a></h3>
+                    </div>
+                    <div className="contact-position">
+                        <img src={CallImage} alt="call-image" className='call-image' />
+                        <h3>(216)-926-6779</h3>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
